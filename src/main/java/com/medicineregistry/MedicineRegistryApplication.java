f4a44ac1efdd91da56f83f6618d463c000362c0c
Alex Shamrai <gibsonshamray@gmail.com>
2022-12-27 22:25:19 +0200

    Initial commit

---
+++ src/main/java/com/medicineregistry/MedicineRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.medicineregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineRegistryApplication.class, args);
+	}
+
+}
